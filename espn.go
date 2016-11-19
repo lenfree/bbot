c7891cb63df0cac007b3019d420e2c76acc74c2d
Lenfree Yeung <lenfree.yeung@gmail.com>
2016-11-19 11:04:00 +1100

    Split and organise code

--- espn.go
+++ espn.go
@@ -1,42 +1,78 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/BeepBoopHQ/go-slackbot"
+	"github.com/nlopes/slack"
+	"github.com/valyala/fasthttp"
+	"golang.org/x/net/context"
+)
+
 type Schedule struct {
-        Date  string `json:"date"`
-        Games []struct {
-                Away struct {
-                        Alias string `json:"alias"`
-                        ID    string `json:"id"`
-                        Name  string `json:"name"`
-                } `json:"away"`
-                AwayPoints int `json:"away_points"`
-                Broadcast  struct {
-                        Network   string `json:"network"`
-                        Satellite string `json:"satellite"`
-                } `json:"broadcast"`
-                Coverage string `json:"coverage"`
-                Home     struct {
-                        Alias string `json:"alias"`
-                        ID    string `json:"id"`
-                        Name  string `json:"name"`
-                } `json:"home"`
-                HomePoints int    `json:"home_points"`
-                ID         string `json:"id"`
-                Scheduled  string `json:"scheduled"`
-                Status     string `json:"status"`
-                Venue      struct {
-                        Address  string `json:"address"`
-                        Capacity int    `json:"capacity"`
-                        City     string `json:"city"`
-                        Country  string `json:"country"`
-                        ID       string `json:"id"`
-                        Name     string `json:"name"`
-                        State    string `json:"state"`
-                        Zip      string `json:"zip"`
-                } `json:"venue"`
-        } `json:"games"`
-        League struct {
-                Alias string `json:"alias"`
-                ID    string `json:"id"`
-                Name  string `json:"name"`
-        } `json:"league"`
+	Date  string `json:"date"`
+	Games []struct {
+		Away struct {
+			Alias string `json:"alias"`
+			ID    string `json:"id"`
+			Name  string `json:"name"`
+		} `json:"away"`
+		AwayPoints int `json:"away_points"`
+		Broadcast  struct {
+			Network   string `json:"network"`
+			Satellite string `json:"satellite"`
+		} `json:"broadcast"`
+		Coverage string `json:"coverage"`
+		Home     struct {
+			Alias string `json:"alias"`
+			ID    string `json:"id"`
+			Name  string `json:"name"`
+		} `json:"home"`
+		HomePoints int    `json:"home_points"`
+		ID         string `json:"id"`
+		Scheduled  string `json:"scheduled"`
+		Status     string `json:"status"`
+		Venue      struct {
+			Address  string `json:"address"`
+			Capacity int    `json:"capacity"`
+			City     string `json:"city"`
+			Country  string `json:"country"`
+			ID       string `json:"id"`
+			Name     string `json:"name"`
+			State    string `json:"state"`
+			Zip      string `json:"zip"`
+		} `json:"venue"`
+	} `json:"games"`
+	League struct {
+		Alias string `json:"alias"`
+		ID    string `json:"id"`
+		Name  string `json:"name"`
+	} `json:"league"`
+}
+
+func NbaScoresHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
+	current_date := time.Now().UTC().Format("2006/01/02")
+
+	url := "http://api.sportradar.us/nba-t3/games/" + current_date + "/schedule.json?api_key=" + espn
+	_, body, err := fasthttp.Get(nil, url)
+	if err != nil {
+		log.Printf("%s", err.Error())
+	}
+	var games Schedule
+	json.Unmarshal(body, &games)
+	for _, g := range games.Games {
+		var homePoints int
+		var awayPoints int
+		if g.AwayPoints == 0 {
+			awayPoints = 0
+		}
+		if g.HomePoints == 0 {
+			homePoints = 0
+		}
+		output := g.Away.Name + " " + strconv.Itoa(awayPoints) + " - " + g.Home.Name + " " + strconv.Itoa(homePoints)
+		bot.Reply(evt, output, slackbot.WithTyping)
+	}
 }
--- main.go
+++ main.go
@@ -1,30 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
-	"strconv"
-	"time"
 
 	"golang.org/x/net/context"
 
 	slackbot "github.com/BeepBoopHQ/go-slackbot"
 	"github.com/joho/godotenv"
 	"github.com/nlopes/slack"
-	"github.com/valyala/fasthttp"
 )
 
 var bot *slackbot.Bot
 var espn string
 
 func init() {
-        err := godotenv.Load()
-         if err != nil {
-           log.Fatal("Error loading .env file")
-         }
-        bot = slackbot.New(os.Getenv("SLACK_TOKEN"))
-        espn = os.Getenv("ESPN_TOKEN")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	bot = slackbot.New(os.Getenv("SLACK_TOKEN"))
+	espn = os.Getenv("ESPN_TOKEN")
 }
 
 func main() {
@@ -33,44 +29,20 @@ func main() {
 	bot.Hear("(?i)how are you(.*)").MessageHandler(HowAreYouHandler)
 	//bot.Hear("(?i)(.*)").MessageHandler(HowAreYouHandler)
 	bot.Hear("(?)attachment").MessageHandler(AttachmentsHandler)
-        bot.Hear("(?i)nba(.*)").MessageHandler(NbaScoresHandler)
+	bot.Hear("(?i)nba(.*)").MessageHandler(NbaScoresHandler)
 	bot.Run()
 }
 
-func NbaScoresHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-        current_date := time.Now().UTC().Format("2006/01/02")
-
-        url := "http://api.sportradar.us/nba-t3/games/" + current_date + "/schedule.json?api_key=" + espn
-        _, body, err := fasthttp.Get(nil, url)
-        if err != nil {
-                log.Printf("%s", err.Error())
-        }
-        var games Schedule
-        json.Unmarshal(body, &games)
-        for _, g := range games.Games {
-                var homePoints int
-                var awayPoints int
-                if g.AwayPoints == 0 {
-                        awayPoints = 0
-                }
-                if g.HomePoints == 0 {
-                        homePoints = 0
-                }
-                output := g.Away.Name + " "+ strconv.Itoa(awayPoints) + " - " + g.Home.Name + " " + strconv.Itoa(homePoints)
-                bot.Reply(evt, output, slackbot.WithTyping)
-        }
-}
-
 func HelloHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-        log.Println(evt)
+	log.Println(evt)
 	bot.Reply(evt, "Oh hello!", slackbot.WithTyping)
 }
 
 func HowAreYouHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-	bot.Reply(evt, "A bit tired, " + evt.User + ". You get it? A bit?", slackbot.WithTyping)
-        log.Printf("%+#v", evt)
-        log.Printf("%+#v", bot)
-        log.Printf("%+#v", evt.User)
+	bot.Reply(evt, "A bit tired, "+evt.User+". You get it? A bit?", slackbot.WithTyping)
+	log.Printf("%+#v", evt)
+	log.Printf("%+#v", bot)
+	log.Printf("%+#v", evt.User)
 }
 
 func AttachmentsHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
